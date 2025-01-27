50b061f17b6e746b746c66e8b0d46ee8927a76b3
KUSUMA.M <kusumamkusu548@gmail.com>
2025-01-27 19:28:28 +0530

    added solution for Course Schedule IV

---
+++ Course Schedule IV.py
@@ -0,0 +1,41 @@
+'''There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course ai first if you want to take course bi.
+For example, the pair [0, 1] indicates that you have to take course 0 before you can take course 1.
+Prerequisites can also be indirect. If course a is a prerequisite of course b, and course b is a prerequisite of course c, then course a is a prerequisite of course c.
+You are also given an array queries where queries[j] = [uj, vj]. For the jth query, you should answer whether course uj is a prerequisite of course vj or not.
+Return a boolean array answer, where answer[j] is the answer to the jth query.
+
+Example 1:
+Input: numCourses = 2, prerequisites = [[1,0]], queries = [[0,1],[1,0]]
+Output: [false,true]
+Explanation: The pair [1, 0] indicates that you have to take course 1 before you can take course 0.
+Course 0 is not a prerequisite of course 1, but the opposite is true.'''
+
+class Solution(object):
+    def checkIfPrerequisite(self, numCourses, prerequisites, queries):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :type queries: List[List[int]]
+        :rtype: List[bool]
+        """
+        adj={i:set() for i in range(numCourses)}
+        for course,pre in prerequisites:
+            adj[course].add(pre)
+        res=[]
+        def dfs(course,target,visited):
+            if course==target:
+                return True
+            if course in visited:
+                return False
+            visited.add(course)
+            for prereq in adj[course]:
+                if dfs(prereq,target,visited):
+                    return True
+            return False
+        for course,pre in queries:
+            visited=set()
+            res.append(dfs(course,pre,visited))
+        return res
+
+            
+        
